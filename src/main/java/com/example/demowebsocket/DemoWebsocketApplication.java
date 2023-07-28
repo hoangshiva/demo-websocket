260ac45cd3fee8b9b33eb017d1c2d222c3e089d3
MSI 14 <hoang6568@gmail.com>
2023-07-28 09:35:29 +0700

    update master2

--- src/main/java/com/example/demowebsocket/DemoWebsocketApplication.java
+++ src/main/java/com/example/demowebsocket/DemoWebsocketApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoWebsocketApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(DemoWebsocketApplication.class, args);
     }
-
 }
